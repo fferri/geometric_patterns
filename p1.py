5bd48713a6250ac37ffa9df3bd9f609112db4e0d
Federico Ferri <federico.ferri.it@gmail.com>
2016-10-17 21:29:29 +0200

    shorten code

--- p1.py
+++ p1.py
@@ -5,9 +5,8 @@ for i in range(6):
     im[[0,-1],...]^=1
     im[...,[0,-1]]^=1
     s//=2
-    c=checkerboard(s,s//2)
-    ch=imtile(c,(c.shape[0],im.shape[1]))
-    cv=imtile(c,(im.shape[0]+2*c.shape[0],c.shape[1]))
+    ch=checkerboard((s,im.shape[1]),s//2)
+    cv=checkerboard((im.shape[0]+2*s,s),s//2)
     im=np.hstack((cv,np.vstack((ch,im,ch)),cv))
 imshow(im)
 imsave(im,'p1.png')
