08ceffbacb5bbfa71b146f349efd121d45882120
Federico Ferri <federico.ferri.it@gmail.com>
2016-10-18 13:57:21 +0200

    add weird spiral

---
+++ p12.py
@@ -0,0 +1,11 @@
+from common import *
+
+w,h=2048,2048
+x,y=np.meshgrid(range(w),range(h))
+r=np.sqrt((x-w/2)**2+(y-h/2)**2)
+a=np.arctan2(x-w/2,y-h/2)
+im1=np.sin(np.log(1+r)*math.pi*16)>0
+im2=np.sin(4*math.pi*np.cos(a*8+8*np.log(1+r)))>0
+im=im1^im2
+imshow(im)
+imsave(im,'p12.png')
