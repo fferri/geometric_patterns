d4346257a4f8c1dd8a7fe307435c7a9366880982
Federico Ferri <federico.ferri.it@gmail.com>
2016-10-23 02:39:09 +0200

    improve p18

--- p18.py
+++ p18.py
@@ -1,7 +1,17 @@
 from common import *
 # inspired by a Aldous Huxley's book cover
-r,a=meshgrid_polar((2048,)*2)
+imgsz=(2048,)*2
+r,a=meshgrid_polar(imgsz)
 im=np.fmod(np.float32(3.5*np.log(1+r))+2*np.power(np.abs(np.sin(8*np.float32(a))),0.4),1.4)
 im=apply_colormap(im,colormap.rainbow)
+def warp(o):
+    def f(i,j):
+        cy,cx=imgsz[0]//2,imgsz[1]//2
+        y,x=i-cy,j-cx
+        r,a=np.sqrt(x**2+y**2),np.arctan2(y,x)
+        return cy+r*np.sin(a+o),cx+r*np.cos(a+o)
+    return f
+im[:,:,0]=imwarp(im[:,:,0],warp(-0.02),cycle)
+im[:,:,2]=imwarp(im[:,:,2],warp(0.03),cycle)
 imshow(im)
 imsave(im,'p18.png')
