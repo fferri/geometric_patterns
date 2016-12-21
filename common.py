2983ee31ff269c78c00501f6bcf53553efbcf233
Federico Ferri <federico.ferri.it@gmail.com>
2016-12-21 10:49:20 +0100

    add bresenham algorithm for drawing lines

--- common.py
+++ common.py
@@ -163,3 +163,42 @@ class colormap:
             cmap[int(p-w/2):int(p+w/2)+1,:]=[255,255,255]
         return cmap
 
+def mkline(start, end):
+    # Bresenham's Line Algorithm
+    x1, y1 = start
+    x2, y2 = end
+    x1, y1, x2, y2 = map(int, (x1, y1, x2, y2))
+    dx = x2 - x1
+    dy = y2 - y1
+    is_steep = abs(dy) > abs(dx)
+    if is_steep:
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+    swapped = False
+    if x1 > x2:
+        x1, x2 = x2, x1
+        y1, y2 = y2, y1
+        swapped = True
+    dx = x2 - x1
+    dy = y2 - y1
+    error = int(dx / 2.0)
+    ystep = 1 if y1 < y2 else -1
+    y = y1
+    X, Y = [], []
+    for x in range(x1, x2 + 1):
+        X.append(y if is_steep else x)
+        Y.append(x if is_steep else y)
+        error -= abs(dy)
+        if error < 0:
+            y += ystep
+            error += dx
+    if swapped:
+        X.reverse()
+        Y.reverse()
+    return X, Y
+
+def imline(im, start, end, value=255, alpha=1.0):
+    x, y = mkline(start, end)
+    x, y = x[1:], y[1:]
+    im[x,y] = alpha*value + (1-alpha)*im[x,y]
+
